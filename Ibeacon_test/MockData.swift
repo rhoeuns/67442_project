cf10e17435e88f7a0618dd3453a4570e00bd5827
awang1 <awang1@andrew.cmu.edu>
2016-11-18 18:37:30 -0500

    Create empty mock data file

---
+++ Ibeacon_test/MockData.swift
@@ -0,0 +1,13 @@
+//
+//  MockData.swift
+//  Ibeacon_test
+//
+//  Created by Alex Wang on 11/18/16.
+//  Copyright © 2016 RhoEun Song. All rights reserved.
+//
+
+import Foundation
+
+class MockData {
+    
+}
