121820c0f4070770e0e65d1f69f320c127b54f0c
awang1 <awang1@andrew.cmu.edu>
2016-11-20 15:19:26 -0500

    Clean up the DataStore comments a bit.

--- Ibeacon_test/DataStore.swift
+++ Ibeacon_test/DataStore.swift
@@ -12,15 +12,7 @@ import SwiftyJSON
 class DataStore {
     var restaurants = [Restaurant]()
     var restaurantsInLineFor = [InLineFor]()
-    
-    
-    // The way I'm doing this right now, Alamofire returns the result and I save it
-    // However, there's no way to tell the rest of the app that the data has changed.
-    // The idea I had was to basically chain callbacks, but I'm not 100% sure how to do that here.
-    
-    // Implemented something below, let's see if it does what I want lol
-    
-    
+
     /**
      Fetches json data payload from the server.
      Then parses the JSON into model objects that will be stored here.
@@ -32,17 +24,6 @@ class DataStore {
         let serverRequestor = ServerRequestor()
         serverRequestor.getPayload(storeResults(completionHandler))
     }
-    
-
-    // Callback completion handler
-//    func storeResults(json: JSON?, error: NSError?) {
-//        let jsonParser = JSONParser()
-//        let models = jsonParser.parseJSON(json!)
-//        
-//        restaurants = models.0
-//        restaurantsInLineFor = models.1
-//    }
-    
     
     /**
      Creates a function to be the completion handler.
