9ee19ad8cde9d640cb8cedfa8c8d96481224bc5d
awang1 <awang1@andrew.cmu.edu>
2016-11-20 02:52:00 -0500

    Create class to handle sending server requests

---
+++ Ibeacon_test/ServerRequestor.swift
@@ -0,0 +1,14 @@
+//
+//  ServerRequestor.swift
+//  Ibeacon_test
+//
+//  Created by Alex Wang on 11/20/16.
+//  Copyright © 2016 RhoEun Song. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ServerRequestor {
+    
+}
