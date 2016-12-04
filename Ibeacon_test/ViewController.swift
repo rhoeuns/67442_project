404619edd29677f937f199fe2e3a22e4ac90da1b
RhoEun Song <rhoeuns@andrew.cmu.edu>
2016-12-04 13:53:45 -0500

    .

--- Ibeacon_test/ViewController.swift
+++ Ibeacon_test/ViewController.swift
@@ -13,30 +13,34 @@ import CoreBluetooth
 class ViewController: UIViewController, CBPeripheralManagerDelegate {
     
     @IBOutlet weak var beaconStatus: UILabel!
+    let dataStore = DataStore()
     
     let locationManager = CLLocationManager()
     let myBTManager = CBPeripheralManager()
     var lastStage = CLProximity.Unknown
     
-    let dataStore = DataStore()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
         // Define in iBeacon.swift
         self.setupBeacon()
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+//    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [AnyObject], inRegion region: CLBeaconRegion) {
+//        print(beacons)
+//    }
     
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
     func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
         
         if peripheral.state == CBPeripheralManagerState.PoweredOff {
             
             simpleAlert("Beacon", message: "Turn On Your Device Bluetooh")
         }        
-    }
 }
