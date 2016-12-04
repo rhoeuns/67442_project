09950dde5eb4b6a1f4d3bd46fbab3809da933a07
awang1 <awang1@andrew.cmu.edu>
2016-12-04 03:23:21 -0500

    Working on getting a data store method for all possible server actions. Not Complete

--- Ibeacon_test/DataStore.swift
+++ Ibeacon_test/DataStore.swift
@@ -10,6 +10,9 @@ import Foundation
 import SwiftyJSON
 
 class DataStore {
+    let serverRequestor = ServerRequestor()
+    let jsonParser = JSONParser()
+    
     var restaurants = [Restaurant]()
 
     /**
@@ -20,7 +23,6 @@ class DataStore {
      - Parameter completionHandler: Optional callback that is called after data is returned and parsed.
      */
     func fetchServerData(completionHandler: (() -> Void)?) {
-        let serverRequestor = ServerRequestor()
         serverRequestor.getRestaurants(storeResults(completionHandler))
     }
     
@@ -34,14 +36,81 @@ class DataStore {
      */
     private func storeResults(completionHandler: (() -> Void)?) -> (JSON?, NSError?) -> Void {
         return { json, error in
-            let jsonParser = JSONParser()
-            let restaurants = jsonParser.parseJSON(json!)
+            
+            let restaurants = self.jsonParser.parseRestaurants(json!)
             
             self.restaurants = restaurants
             
             if completionHandler != nil {
                 completionHandler!()
             }
+        }
+    }
+    
+    
+    func updateRestaurants(completionHandler: (() -> Void)?) {
+        serverRequestor.getRestaurants() { json, error in
+            let restaurants = self.jsonParser.parseRestaurants(json!)
+            self.restaurants = restaurants
+            
+            if completionHandler != nil {
+                completionHandler!()
+            }
+        }
+    }
+    
+    func updateRestaurant(restaurant: Restaurant, completionHandler: (() -> Void)?) {
+        serverRequestor.getRestaurant(restaurant.id) { json, error in
+            let updatedRestaurant = self.jsonParser.parseRestaurant(json!)
+            var resultRestaurant = restaurant // this will hold the final result that is returned
+            
+            // Specifically checking and updating the wait times only
+            
+            //// check general time updated
+            //// check personal time null
+            //// check personal time updated
+            //// NOTE: time that is in the future is an update, any time before now is NOT
+            if self.timeWasUpdated(restaurant.generalEstimatedSeatingTime, newTime: updatedRestaurant.generalEstimatedSeatingTime) {
+                resultRestaurant.generalEstimatedSeatingTime = updatedRestaurant.generalEstimatedSeatingTime
+            }
+            
+            if restaurant.personal_estimated_seating_time == nil && updatedRestaurant.personal_estimated_seating_time != nil {
+                // if there has been a reservation made since last time
+                resultRestaurant.personal_estimated_seating_time = updatedRestaurant.personal_estimated_seating_time
+            }
+            else if restaurant.personal_estimated_seating_time != nil && updatedRestaurant.personal_estimated_seating_time == nil {
+                // if there has been a cancelled reservation since last time
+                resultRestaurant.personal_estimated_seating_time = nil
+            }
+            else {
+                if let oldPersonal = restaurant.personal_estimated_seating_time,
+                    newPersonal = updatedRestaurant.personal_estimated_seating_time {
+                    if self.timeWasUpdated(oldPersonal, newTime: newPersonal) {
+                        resultRestaurant.personal_estimated_seating_time = updatedRestaurant.personal_estimated_seating_time
+                    }
+                }
+            }
+            
+            // TODO: probably need to have a specific way format for the completion handler
+            //       so that you know IF an update happened, and if so what kind
+            if completionHandler != nil {
+                completionHandler!()
+            }
+        }
+    }
+    
+    private func timeWasUpdated(oldTime: NSDate, newTime: NSDate) -> Bool {
+        if oldTime.timeIntervalSinceNow < 0 && newTime.timeIntervalSinceNow < 0 {
+            // if both times are earlier than the current date and time
+            // that is equivalent to there being no wait then and no wait now
+            return false
+        }
+        else if oldTime.timeIntervalSinceDate(newTime) != 0 {
+            // if there is a difference between the times
+            return true
+        }
+        else {
+            return false
         }
     }
     
